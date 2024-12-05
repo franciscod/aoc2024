859b899e801800846c2cdc6e25e60c088e53616b
Francisco Demartino <git@fd1.com.ar>
2024-12-05 02:15:05 -0300

    d5p1

---
+++ 5/solve.py
@@ -0,0 +1,38 @@
+import fileinput
+from collections import defaultdict
+
+rules = []
+prods = []
+
+after = defaultdict(set)
+
+for line in fileinput.input():
+    line = line.strip()
+    if line == '':
+        continue
+    if '|' in line:
+        rules.append(tuple(map(int, line.split('|'))))
+    else:
+        prods.append(list(map(int, line.split(','))))
+
+for x, y in rules:
+    after[x].add(y)
+    print(x, y)
+
+
+def check(upd):
+    print("CHECKING", upd)
+    for i in range(0, len(upd)-1):
+        for j in range(i+1, len(upd)):
+            a, b = upd[i], upd[j]
+            print(a, b)
+            if a in after[b]:
+                return False
+    return True
+
+tot = 0
+for upd in prods:
+    if check(upd):
+        tot += upd[len(upd)//2]
+
+print(tot)
